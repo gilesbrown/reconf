c697ff3cce58232299fb552229c9172dbb623078
Giles Brown <gsbrown@ebay.com>
2015-07-30 11:32:46 -0700

    Version 0.1.4

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup
 
-version = '0.1.3'
+version = '0.1.4'
 
 setup(
     name='reconf',
@@ -9,7 +9,7 @@ setup(
     author='Giles Brown',
     author_email='giles_brown@hotmail.com',
     url='http://github.com/gilesbrown/reconf',
-    download_url = 'http://github.com/gilesbrown/reconf/tarball/' + version,
+    download_url='http://github.com/gilesbrown/reconf/tarball/' + version,
     license='BSD',
     packages=['reconf'],
     install_requires=[
@@ -22,7 +22,5 @@ setup(
         'License :: OSI Approved :: BSD License',
         'Programming Language :: Python',
         'Programming Language :: Python :: 2.7',
-        #'Programming Language :: Python :: 3',
-        #'Programming Language :: Python :: 3.3',
     ),
 )
